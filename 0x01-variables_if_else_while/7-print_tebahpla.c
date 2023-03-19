f85d5a4f3cb5bc7b1336e120af86fb527e3d3678
crestFall32 <ewajenrola@gmail.com>
2023-03-19 16:55:55 -0700

    Reverse a-z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet using putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
