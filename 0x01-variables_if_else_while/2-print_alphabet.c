7ebea63b6839cb87a95fa6c49bd8837f4c66dc4c
crestFall32 <ewajenrola@gmail.com>
2023-03-19 05:57:03 -0700

    Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet using putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	while (alpha[i] != \0)
+	{
+		putchar(alpha[i]);
+		i++
+	}
+return 0
+}
