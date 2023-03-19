d0c2428b688627f66af44dafb486514cf3e4ab6d
crestFall32 <ewajenrola@gmail.com>
2023-03-19 15:05:43 -0700

    Alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
- * main - print the alphabet, except q and e, using putchar
+ * main - print the alphabet using putchar
  * Return: 0 (success)
  */
 
 int main(void)
 {
 	char c;
-
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c != 'q' &&c != 'e')
+		if ( c != 'q' && c != 'e')
 		{
 			putchar(c);
 		}
 	}
-	putchar(\n);
-
-	return (0)
+	putchar('\n');
+	return (0);
 }
