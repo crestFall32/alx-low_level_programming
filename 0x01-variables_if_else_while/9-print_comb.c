d23f713ff0c04193465bd97b813a000bc08dff49
crestFall32 <ewajenrola@gmail.com>
2023-03-20 13:52:46 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0', n < '10', n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(",");
+			puchar(" ");
+		}
+	}
+	return (0);
+}
