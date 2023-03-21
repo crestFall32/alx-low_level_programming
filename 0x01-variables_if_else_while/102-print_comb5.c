79e165e47fb6787fb2c5c7865ca8ddc588ce7ee9
crestFall32 <ewajenrola@gmail.com>
2023-03-20 21:33:56 -0700

    Debug perm3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 100; n1++)
+	{
+		for (n2 = 0; n2 < 100; n2++)
+		{
+			if (n2 > n1)
+			{
+				putchar((n1/10) + '0');
+				putchar((n1%10) + '0');
+				putchar(' ');
+				putchar((n2/10) + '0');
+				putchar((n2/10) + '0');
+				if (n1 != 98 || n2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('/n');
+	return (0);
+}
