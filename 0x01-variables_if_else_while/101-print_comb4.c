59a15d163a996137d0cc8dcf98dafad11515e0b1
crestFall32 <ewajenrola@gmail.com>
2023-03-20 20:19:05 -0700

    Success combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digit
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int i, j, n;
+
+	while (i != j && j != n && i != n)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			for (j = i + 1; j < 9; j++)
+			{
+				for (n = j + 1; n < 10; n++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(n + '0');
+					if (i != '7' || j != '8' || n != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
