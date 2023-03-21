e9e2bddb6790e42866ecf8521552c373c5c14303
crestFall32 <ewajenrola@gmail.com>
2023-03-20 17:05:37 -0700

    Permutation

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all possible different
+ * combinations of two digits.
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		for (n = 0; n < 10; n++)
+		{
+			putchar(n + '0');
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
