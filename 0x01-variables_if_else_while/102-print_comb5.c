bc718445a631a3fb7009626cf30f0173cbe197eb
crestFall32 <ewajenrola@gmail.com>
2023-03-20 21:38:33 -0700

    debug 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 		{
 			if (n2 > n1)
 			{
-				putchar((n1/10) + '0');
-				putchar((n1%10) + '0');
+				putchar((n1 / 10) + '0');
+				putchar((n1 % 10) + '0');
 				putchar(' ');
-				putchar((n2/10) + '0');
-				putchar((n2/10) + '0');
+				putchar((n2 / 10) + '0');
+				putchar((n2 % 10) + '0');
 				if (n1 != 98 || n2 != 99)
 				{
 					putchar(',');
