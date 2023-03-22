c10c2dda040db9e4ad72a3d16a824224cd34d5c4
crestFall32 <ewajenrola@gmail.com>
2023-03-22 15:49:59 -0700

    last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
  * print_last_digit - prints the last digit of a number
+ * @n: number
+ *
+ * Return: n, now the last number
  */
 int print_last_digit(int n)
 {
 	n %= 10;
-	printf("%d", n);
+	return (n);
 }
