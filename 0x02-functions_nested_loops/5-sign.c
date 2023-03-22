180d9959f0da2f1e5dafc86e0d137c0a840dbc5f
crestFall32 <ewajenrola@gmail.com>
2023-03-22 14:13:55 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * print_sign - prints the sign of a number
- *@n - tested number
+ * @n: tested integer
+ *
+ * Return: 1 if positive, 0 if zero and -1 if negative
  */
 int print_sign(int n)
 {
