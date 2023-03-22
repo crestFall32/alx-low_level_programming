c3bc092e2799b470748bf4dc4edceaa786e38e8e
crestFall32 <ewajenrola@gmail.com>
2023-03-21 21:15:11 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a function for ABCs
+ * Return: 0 is success
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l ='a'; l <= 'z'; l++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
